009444c6f68d6779c74dc5eef116025aef9a748c
Savo Kostadinov <80397243+savokostadinov@users.noreply.github.com>
2024-08-13 11:06:53 +0200

    Delete scripts2 directory
    Deleted unnecessary directory

--- scripts2/config/config.js
+++
@@ -1,3 +0,0 @@
-// config/config.js
-export const BASE_URL = 'https://thinking-tester-contact-list.herokuapp.com';
-export const API_URL = 'https://thinking-tester-contact-list.herokuapp.com/api';
--- scripts2/test_data/test_data.js
+++
@@ -1,7 +0,0 @@
-// test_data/test_data.js
-export const contactData = {
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'john.doe@example.com',
-    phone: '123-456-7890',
-};
